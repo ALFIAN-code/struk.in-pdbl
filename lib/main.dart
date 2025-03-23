8cb53b2a4f8b7d93d34060dce6b09a9f3bca94e7
ALFIAN-code <allvvnt@gmail.com>
2025-03-23 02:22:50 -0400

    fix logo

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.white, // Warna utama
+      ),
       debugShowCheckedModeBanner: false,
       home: LayoutBuilder(
         builder: (context, constraints) {
